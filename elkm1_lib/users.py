67e2018544b4ffce08487bc1642f84c096840f34
Glenn Waters <gwwaters@gmail.com>
2018-08-30 13:09:42 -0400

    Set user name to default name if not configured

--- elkm1_lib/users.py
+++ elkm1_lib/users.py
@@ -1,4 +1,7 @@
 """Definition of an ElkM1 User"""
+
+import re
+
 from .const import Max, TextDescriptions
 from .elements import Element, Elements
 
@@ -16,3 +19,11 @@ class Users(Elements):
     def sync(self):
         """Retrieve areas from ElkM1"""
         self.get_descriptions(TextDescriptions.USER.value)
+
+    def _got_desc(self, descriptions):
+        super()._got_desc(descriptions)
+        # Elk returns all user descriptions rather than just configured users
+        for user in self.elements:
+            if re.match(r'^USER \d\d\d', user.name):
+                user.name = user.default_name()
+

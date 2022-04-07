7f2350bd4176da8241d0624e8f6dfaf62adc63e7
Glenn Waters <glenn@watrs.ca>
2022-04-07 12:35:34 -0400

    Initial typing for elk.py

--- elkm1_lib/elk.py
+++ elkm1_lib/elk.py
@@ -1,13 +1,16 @@
 """Master class that combines all ElkM1 pieces together."""
 
+from __future__ import annotations
+
 import asyncio
 import logging
 from functools import partial
 from importlib import import_module
+from typing import Any, Optional
 
 import serial_asyncio
 
-from .message import MessageDecode, ua_encode
+from .message import MessageDecode, MessageEncode, MsgHandler, ua_encode
 from .proto import Connection
 from .util import parse_url, url_scheme_is_secure
 
@@ -17,11 +20,17 @@ LOG = logging.getLogger(__name__)
 class Elk:  # pylint: disable=too-many-instance-attributes
     """Represents all the components on an Elk panel."""
 
-    def __init__(self, config, loop=None):
+    # For typing dynamic attrs in element_list
+    # def __setattr__(self, name: str, value: Any) -> None:
+    #     super().__setattr__(name, value)
+    #
+    def __init__(
+        self, config: dict[str, Any], loop: asyncio.AbstractEventLoop | None = None
+    ) -> None:
         """Initialize a new Elk instance."""
         self.loop = loop if loop else asyncio.get_event_loop()
         self._config = config
-        self._connection = None
+        self._connection: Optional[Connection] = None
         self._connection_retry_time = 1
         self._message_decode = MessageDecode()
         self._reconnect_task = None
@@ -52,18 +61,18 @@ class Elk:  # pylint: disable=too-many-instance-attributes
             class_ = getattr(module, element.capitalize())
             setattr(self, element, class_(self))
 
-    def _sync_complete(self, **kwargs):  # pylint: disable=unused-argument
+    def _sync_complete(self, **kwargs: dict) -> None:  # pylint: disable=unused-argument
         self._message_decode.call_handlers("sync_complete", {})
 
         # So that other apps can send UA and not trigger sync_complete
         self.remove_handler("UA", self._sync_complete)
 
-    def _login_status(self, succeeded):
+    def _login_status(self, succeeded: bool) -> None:
         if not succeeded:
             self.disconnect()
             LOG.error("Invalid username or password.")
 
-    async def _connect(self):
+    async def _connect(self) -> None:
         """Asyncio connection to Elk."""
         url = self._config["url"]
         LOG.info("Connecting to ElkM1 at %s", url)
@@ -102,19 +111,19 @@ class Elk:  # pylint: disable=too-many-instance-attributes
             )
             self._start_connection_retry_timer()
 
-    def _start_connection_retry_timer(self):
+    def _start_connection_retry_timer(self) -> None:
         timeout = self._connection_retry_time
         if timeout > 0:
             self._reconnect_task = self.loop.call_later(timeout, self._reconnect)
             self._connection_retry_time = min(60, timeout * 2)
 
-    def _connected(self, connection):
+    def _connected(self, connection: Connection) -> None:
         """Login and sync the ElkM1 panel to memory."""
         LOG.info("Connected to ElkM1")
         self._connection = connection
         self._connection_retry_time = 1
         self._message_decode.call_handlers("connected", {})
-        if url_scheme_is_secure(self._config["url"]):
+        if connection and url_scheme_is_secure(self._config["url"]):
             self._connection.write_data(self._config["userid"], raw=True)
             self._connection.write_data(self._config["password"], raw=True)
         else:
@@ -122,34 +131,34 @@ class Elk:  # pylint: disable=too-many-instance-attributes
 
         self.call_sync_handlers()
 
-    def _reconnect(self):
+    def _reconnect(self) -> None:
         asyncio.ensure_future(self._connect())
 
-    def _disconnected(self):
+    def _disconnected(self) -> None:
         LOG.warning("ElkM1 at %s disconnected", self._config["url"])
         self._connection = None
         self._start_connection_retry_timer()
         self._message_decode.call_handlers("disconnected", {})
 
-    def add_handler(self, msg_type, handler):
+    def add_handler(self, msg_type: str, handler: MsgHandler) -> None:
         """Add handler."""
         self._message_decode.add_handler(msg_type, handler)
 
-    def remove_handler(self, msg_type, handler):
+    def remove_handler(self, msg_type: str, handler: MsgHandler) -> None:
         """Remove handler."""
         self._message_decode.remove_handler(msg_type, handler)
 
-    def _got_data(self, data):
+    def _got_data(self, data: str) -> None:
         LOG.debug("got_data '%s'", data)
         try:
             self._message_decode.decode(data)
         except (ValueError, AttributeError) as exc:
             LOG.error("Invalid message '%s'", data, exc_info=exc)
 
-    def _timeout(self, msg_code):
+    def _timeout(self, msg_code: str) -> None:
         self._message_decode.call_handlers("timeout", {"msg_code": msg_code})
 
-    def call_sync_handlers(self):
+    def call_sync_handlers(self) -> None:
         """Invoke the synchronization handlers."""
         LOG.debug("Synchronizing panel...")
 
@@ -157,34 +166,34 @@ class Elk:  # pylint: disable=too-many-instance-attributes
             getattr(self, element).sync()
         self.send(ua_encode(0))  # Used to mark end of sync
 
-    def is_connected(self):
+    def is_connected(self) -> bool:
         """Status of connection to Elk."""
         return self._connection is not None
 
-    def connect(self):
+    def connect(self) -> None:
         """Connect to the panel"""
         asyncio.ensure_future(self._connect())
 
-    def run(self):
+    def run(self) -> None:
         """Enter the asyncio loop."""
         self.loop.run_forever()
 
-    def send(self, msg):
+    def send(self, msg: MessageEncode) -> None:
         """Send a message to Elk panel."""
         if self._connection:
             self._connection.write_data(msg.message, msg.response_command)
 
-    def pause(self):
+    def pause(self) -> None:
         """Pause the connection from sending/receiving."""
         if self._connection:
             self._connection.pause()
 
-    def resume(self):
+    def resume(self) -> None:
         """Restart the connection from sending/receiving."""
         if self._connection:
             self._connection.resume()
 
-    def disconnect(self):
+    def disconnect(self) -> None:
         """Disconnect the connection from sending/receiving."""
         self._connection_retry_time = -1  # Stop future timeout reconnects
         if self._connection:

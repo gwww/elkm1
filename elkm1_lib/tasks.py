1f0872824d24be9a8ae6f46c980ef5a03a5d92fd
Glenn Waters <glenn@watrs.ca>
2022-04-07 13:53:20 -0400

    Initial typing for tasks.py

--- elkm1_lib/tasks.py
+++ elkm1_lib/tasks.py
@@ -3,17 +3,20 @@ from time import time
 
 from .const import Max, TextDescriptions
 from .elements import Element, Elements
+from .elk import Elk
 from .message import tn_encode
 
 
 class Task(Element):
     """Class representing an Task"""
 
-    def __init__(self, index, elk):  # pylint: disable=useless-super-delegation
+    def __init__(
+        self, index: int, elk: Elk
+    ) -> None:  # pylint: disable=useless-super-delegation
         super().__init__(index, elk)
         self.last_change = None
 
-    def activate(self):
+    def activate(self) -> None:
         """(Helper) Activate task"""
         self._elk.send(tn_encode(self._index))
 
@@ -21,13 +24,13 @@ class Task(Element):
 class Tasks(Elements):
     """Handling for multiple tasks"""
 
-    def __init__(self, elk):
+    def __init__(self, elk: Elk) -> None:
         super().__init__(elk, Task, Max.TASKS.value)
         elk.add_handler("TC", self._tc_handler)
 
-    def sync(self):
+    def sync(self) -> None:
         """Retrieve tasks from ElkM1"""
         self.get_descriptions(TextDescriptions.TASK.value)
 
-    def _tc_handler(self, task):
+    def _tc_handler(self, task: int) -> None:
         self.elements[task].setattr("last_change", time(), True)

d8822ccdaa9b8e07cb68b74054163b93471dd400
Glenn Waters <glenn@watrs.ca>
2022-04-07 00:20:20 -0400

    Initial typing for users.py

--- elkm1_lib/users.py
+++ elkm1_lib/users.py
@@ -2,21 +2,22 @@
 
 from .const import Max, TextDescriptions
 from .elements import Element, Elements
+from .elk import Elk
 
 
 class User(Element):
     """Class representing an User"""
 
-    def __init__(self, index, elk):  # pylint: disable=useless-super-delegation
+    def __init__(self, index: int, elk: Elk) -> None:  # pylint: disable=useless-super-delegation
         super().__init__(index, elk)
 
 
 class Users(Elements):
     """Handling for multiple areas"""
 
-    def __init__(self, elk):
+    def __init__(self, elk: Elk) -> None:
         super().__init__(elk, User, Max.USERS.value)
 
-    def sync(self):
+    def sync(self) -> None:
         """Retrieve areas from ElkM1"""
         self.get_descriptions(TextDescriptions.USER.value)

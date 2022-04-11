96e739c55db75ac072adb8f041c894fd904e2b6a
Glenn Waters <glenn@watrs.ca>
2022-04-11 15:57:32 -0400

    Move username fn from util to a method in Users

--- elkm1_lib/users.py
+++ elkm1_lib/users.py
@@ -1,5 +1,7 @@
 """Definition of an ElkM1 User"""
 
+from typing import cast
+
 from .const import Max, TextDescriptions
 from .elements import Element, Elements
 from .elk import Elk
@@ -18,3 +20,15 @@ class Users(Elements):
     def sync(self) -> None:
         """Retrieve areas from ElkM1"""
         self.get_descriptions(TextDescriptions.USER.value)
+
+    def username(self, user_number: int) -> str:
+        """Return name of user."""
+        if user_number >= 0 and user_number < self.max_elements:
+            return self.elements[user_number].name
+        if user_number == 201:
+            return "*Program*"
+        if user_number == 202:
+            return "*Elk RP*"
+        if user_number == 203:
+            return "*Quick arm*"
+        return ""

be7a8b7268f6590ba21436ccc7321c2dd7fd5156
Glenn Waters <glenn@watrs.ca>
2022-04-30 00:53:19 -0400

    Fix tests from refactor.

--- test/test_connection.py
+++ test/test_connection.py
@@ -7,21 +7,21 @@ from elkm1_lib.connection import _ElkProtocol
 
 def test_receive_single_packet_and_pass_to_handler():
     mock_gotdata = Mock()
-    connection = _ElkProtocol(None, 0, None, None, mock_gotdata, None)
+    connection = _ElkProtocol(Mock(), 0, Mock(), Mock(), mock_gotdata, Mock())
     connection.data_received("test\r\n".encode())
     mock_gotdata.assert_called_once_with("test")
 
 
 def test_receive_multiple_packets():
     mock_gotdata = Mock()
-    connection = _ElkProtocol(None, 0, None, None, mock_gotdata, None)
+    connection = _ElkProtocol(Mock(), 0, Mock(), Mock(), mock_gotdata, Mock())
     connection.data_received("test1\r\ntest2\r\n".encode())
     mock_gotdata.assert_has_calls([call("test1"), call("test2")])
 
 
 def test_received_full_packet_and_partial_packet():
     mock_gotdata = Mock()
-    connection = _ElkProtocol(None, 0, None, None, mock_gotdata, None)
+    connection = _ElkProtocol(Mock(), 0, Mock(), Mock(), mock_gotdata, Mock())
     connection.data_received("test1\r\nfirst-bit,".encode())
     mock_gotdata.assert_called_once_with("test1")
 
@@ -30,7 +30,7 @@ def test_received_full_packet_and_partial_packet():
 
 
 def test_write_simple():
-    connection = _ElkProtocol(None, 0, None, None, None, None)
+    connection = _ElkProtocol(Mock(), 0, Mock(), Mock(), Mock(), Mock())
     connection._transport = Mock()
     connection.write_data("08cv0900")
     connection._transport.write.assert_called_once_with("08cv0900F6\r\n".encode())

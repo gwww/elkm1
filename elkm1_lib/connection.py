ee2a97c54d7bc164afe4208805653bd71ffb4665
Glenn Waters <glenn@watrs.ca>
2022-11-24 20:48:20 -0500

    Rewrite connection.py to be more "async"
    Move away from createing a transport and let the Python library handle
    those details.
    
    No more callbacks inside of connection code.
    
    Faster timer logic by moving to asyncio.Event set/clear.

--- elkm1_lib/connection.py
+++ elkm1_lib/connection.py
@@ -19,8 +19,10 @@ LOG = logging.getLogger(__name__)
 HEARTBEAT_TIME = 120
 MESSAGE_RESPONSE_TIME = 5.0
 
+
 class QueuedWrite(NamedTuple):
-    """Entries in the write queue use this structure."""
+    """Structure for entries in the write queue."""
+
     msg: str
     response_cmd: str | None
     timeout: float = 5.0
@@ -37,60 +39,52 @@ class Connection:
         self._writer: asyncio.StreamWriter | None = None
         self._awaiting_response_command: str | None = None
         self._paused = False
-        self._heartbeat_event = asyncio.Event()
-        self._message_timer_event = asyncio.Event()
         self._write_queue: deque[QueuedWrite] = deque()
         self._check_write_queue = asyncio.Event()
         self._response_received = asyncio.Event()
+        self._heartbeat_event = asyncio.Event()
         self._tasks: set[Optional[asyncio.Task]] = set()
-        self._response_timeout_task: Optional[asyncio.TimerHandle] = None
 
     async def connect(self) -> None:
         """Create connection to Elk."""
 
         LOG.info("Connecting to ElkM1 at %s", self._url)
-        connection_retry_time = 1
+        retry_time = 1
         scheme, dest, param, ssl_context = parse_url(self._url)
-        while True:
+        while not self._writer:
             if scheme == "serial":
-                coro = serial_asyncio.open_serial_connection(port=dest, baudrate=param)
+                coro = serial_asyncio.open_serial_connection(url=dest, baudrate=param)
             else:
                 coro = asyncio.open_connection(host=dest, port=param, ssl=ssl_context)
             try:
                 reader, self._writer = await asyncio.wait_for(coro, timeout=30)
             except (ValueError, OSError, asyncio.TimeoutError) as err:
-                # if self._connection_retry_time <= 0:
-                #     return
                 LOG.warning(
-                    "Could not connect to ElkM1 (%s). Retrying in %d seconds",
+                    "Error connecting to ElkM1 (%s). Retrying in %d seconds",
                     err,
-                    connection_retry_time,
+                    retry_time,
                 )
-                await asyncio.sleep(connection_retry_time)
-                connection_retry_time = min(60, connection_retry_time * 2)
+                await asyncio.sleep(retry_time)
+                retry_time = min(60, retry_time * 2)
                 continue
 
             if scheme != "serial":
                 self._tasks.add(asyncio.create_task(self._heartbeat_timer()))
-            self._tasks.add(asyncio.create_task(self._read(reader)))
-            self._tasks.add(asyncio.create_task(self._write()))
-
+            self._tasks.add(asyncio.create_task(self._read_stream(reader)))
+            self._tasks.add(asyncio.create_task(self._write_stream()))
             self._notifier.notify("connected", {})
-            break
 
-    async def _read(self, reader: asyncio.StreamReader) -> None:
+    async def _read_stream(self, reader: asyncio.StreamReader) -> None:
         read_buffer = ""
         while True:
-            data = await reader.read(1000)
+            data = await reader.read(500)
             if not data:
                 break
-
             self._heartbeat()
 
             read_buffer += data.decode("ISO-8859-1")
             while "\r\n" in read_buffer:
                 line, read_buffer = read_buffer.split("\r\n", 1)
-
                 if get_elk_command(line) == self._awaiting_response_command:
                     self._response_received.set()
                     self._check_write_queue.set()
@@ -102,20 +96,19 @@ class Connection:
                         self._notifier.notify(decoded[0], decoded[1])
                 except (ValueError, AttributeError) as exc:
                     LOG.error("Invalid message '%s'", data, exc_info=exc)
+
         self._tasks.remove(asyncio.current_task())
 
-    async def _write(self) -> None:
+    async def _write_stream(self) -> None:
         while True:
-            await self._check_write_queue.wait()
-            self._check_write_queue.clear()
+            if not self._write_queue:
+                await self._check_write_queue.wait()
+                self._check_write_queue.clear()
+
             if not self._writer:
                 break
 
-            if self._awaiting_response_command or not self._write_queue or self._paused:
-                continue
-
             q_entry = self._write_queue.popleft()
-
             if not q_entry.raw:
                 cksum = (256 - reduce(lambda x, y: x + y, map(ord, q_entry.msg))) % 256
                 msg = f"{q_entry.msg}{cksum:02X}\r\n"
@@ -125,34 +118,34 @@ class Connection:
             LOG.debug("write_data '%s'", msg[:-2])
             self._writer.write((msg).encode())
 
-            if not q_entry.response_cmd:
-                continue
-
-            self._awaiting_response_command = q_entry.response_cmd
-            try:
-                async with async_timeout.timeout(MESSAGE_RESPONSE_TIME):
-                    await self._response_received.wait()
-            except asyncio.TimeoutError:
-                self._notifier.notify("timeout", {"msg_code": q_entry.response_cmd})
-                pass
-            self._response_received.clear()
-            self._awaiting_response_command = None
+            if q_entry.response_cmd:
+                self._awaiting_response_command = q_entry.response_cmd
+                try:
+                    async with async_timeout.timeout(MESSAGE_RESPONSE_TIME):
+                        await self._response_received.wait()
+                except asyncio.TimeoutError:
+                    self._notifier.notify("timeout", {"msg_code": q_entry.response_cmd})
+                self._response_received.clear()
+                self._awaiting_response_command = None
 
         self._tasks.remove(asyncio.current_task())
 
-    def send(self, msg: MessageEncode, priority_send: bool = False) -> None:
-        """Send a message to Elk."""
-        q_entry = QueuedWrite(msg.message, msg.response_command)
+    def _send(self, q_entry: QueuedWrite, priority_send: bool):
+        if self._paused:
+            return
         if priority_send:
             self._write_queue.appendleft(q_entry)
         else:
             self._write_queue.append(q_entry)
         self._check_write_queue.set()
 
+    def send(self, msg: MessageEncode, priority_send: bool = False) -> None:
+        """Send a message to Elk."""
+        self._send(QueuedWrite(msg.message, msg.response_command), priority_send)
+
     def send_raw(self, msg: str) -> None:
         """Send a raw message to Elk (no checksum will be added)."""
-        self._write_queue.append(QueuedWrite(msg, None, raw=True))
-        self._check_write_queue.set()
+        self._send(QueuedWrite(msg, None, raw=True), False)
 
     def is_connected(self) -> bool:
         """Is the connection active?"""
@@ -160,12 +153,11 @@ class Connection:
 
     def pause(self) -> None:
         """Pause the connection from sending/receiving."""
-        # self._cleanup() TODO
         self._write_queue.clear()
         self._paused = True
 
     def resume(self) -> None:
-        """Restart the connection from sending/receiving."""
+        """Restart the connection sending/receiving."""
         self._paused = False
 
     def disconnect(self) -> None:
